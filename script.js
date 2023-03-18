309737a8ee95a588c8da134cbc04594202272b3a
felipe.silva <felipe.silva@ez4.com.br>
2023-03-18 09:30:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor;
+}
+
+function trocaImagem(imagem) {
+    let iphone = document.querySelector("#iphone")
+    iphone.src = (imagem)
+}
